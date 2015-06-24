7797f71ffc89d9a9c51430c2492b89b16ad24e8c
Amr <amr.e.dev@gmail.com>
2015-06-25 00:31:01 +0200

    hack e egg

---
+++ files/easter_egg.py
@@ -0,0 +1,19 @@
+# funny
+# easter eggs
+
+# ask the user for a file
+fname = raw_input("path to file >")
+
+# validate file
+try:
+	fh = open(fname)
+except:
+	print "Invalid path", fname
+	exit()
+
+i = 0
+
+for line in fh:
+	i += 1
+
+print "num of lines: ", i

bcdc9c5a0ecb7e6197e0e107260d22926f0bed77
Amr <amr.e.dev@gmail.com>
2015-06-24 23:45:16 +0200

    pt upperC

---
+++ files/shout.py
@@ -0,0 +1,16 @@
+# read the contents of a file 
+# and print all lns in UpperCase
+
+# ask the user for a file
+fname = raw_input("path to file >")
+
+# validate file
+try:
+	fh = open(fname)
+except:
+	print("Invalid path")
+	exit
+
+# pt all lines
+for line in fh:
+	print(line.upper())
